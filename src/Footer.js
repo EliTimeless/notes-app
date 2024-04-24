1a3299d0f6500d849c31784f326ea8d4b95433c4
EliTimeless <EliTimeless@users.noreply.github.com>
2024-04-24 17:46:33 +0200

    create and adjust new react app

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const updateYear = new Date();
+var year = updateYear.getFullYear();
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Copywright {year}</p>
+    </footer>
+  );
+}
---
+++ src/Notes.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>This is note title</h1>
+      <p>
+        This is note content: Lorem ipsum dolor sit, amet consectetur
+        adipisicing elit. Quae, dolorem voluptates consequatur obcaecati fuga in
+        laboriosam iure. Velit debitis dolores sint ullam, quasi similique.
+        Repellat quis nobis perferendis optio consectetur, accusantium vero
+        recusandae nemo provident. Voluptatibus repudiandae dicta facere labore!
+      </p>
+    </div>
+  );
+}
